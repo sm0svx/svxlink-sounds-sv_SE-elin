b7cb1c9e4872020604ca24721cb69b96f3dd13f1
Tobias Blomberg <sm0svx@users.noreply.github.com>
2014-08-08 22:31:35 +0200

    Added the locale.tcl file to the Swedish language pack

---
+++ events.d/locale.tcl
@@ -0,0 +1,151 @@
+###############################################################################
+#
+# This file contains modifications of the functions to say
+# numbers and time, for the Swedish language.
+#
+###############################################################################
+
+#
+# Spell the specified word using a phonetic alphabet
+#
+proc spellWord {word} {
+  set word [string tolower $word];
+  for {set i 0} {$i < [string length $word]} {set i [expr $i + 1]} {
+    set char [string index $word $i];
+    if {$char == "*"} {
+      playMsg "Default" "star";
+    } elseif {$char == "/"} {
+      playMsg "Default" "slash";
+    } elseif {$char == "-"} {
+      playMsg "Default" "dash";
+    } elseif {[regexp {[a-z0-9]} $char]} {
+      playMsg "Default" "phonetic_$char";
+    }
+  }
+}
+
+
+#
+# Spell the specified number digit for digit
+#
+proc spellNumber {number} {
+  for {set i 0} {$i < [string length $number]} {set i [expr $i + 1]} {
+    set ch [string index $number $i];
+    if {$ch == "."} {
+      playMsg "Default" "decimal";
+    } else {
+      playMsg "Default" "$ch";
+    }
+  }
+}
+
+
+#
+# Say the specified two digit number (00 - 99)
+#
+proc playTwoDigitNumber {number} {
+  if {[string length $number] != 2} {
+    puts "*** WARNING: Function playTwoDigitNumber received a non two digit number: $number";
+    return;
+  }
+  
+  set first [string index $number 0];
+  if {($first == "0") || ($first == "O")} {
+    playMsg "Default" $first;
+    playMsg "Default" [string index $number 1];
+  } elseif {$first == "1"} {
+    playMsg "Default" $number;
+  } elseif {[string index $number 1] == "0"} {
+    playMsg "Default" $number;
+  } else {
+    playMsg "Default" "[string index $number 0]X";
+    playMsg "Default" "[string index $number 1]";
+  }
+}
+
+
+#
+# Say the specified three digit number (000 - 999)
+#
+proc playThreeDigitNumber {number} {
+  if {[string length $number] != 3} {
+    puts "*** WARNING: Function playThreeDigitNumber received a non three digit number: $number";
+    return;
+  }
+  
+  set first [string index $number 0];
+  if {($first == "0") || ($first == "O")} {
+    spellNumber $number
+  } else {
+    append first "00";
+    playMsg "Default" $first;
+    if {[string index $number 1] != "0"} {
+      playTwoDigitNumber [string range $number 1 2];
+    } elseif {[string index $number 2] != "0"} {
+      playMsg "Default" [string index $number 2];
+    }
+  }
+}
+
+
+#
+# Say a number as intelligent as posible. Examples:
+#
+#	1	- one
+#	24	- twentyfour
+#	245	- twohundred and fourtyfive
+#	1234	- twelve thirtyfour
+#	12345	- onehundred and twentythree fourtyfive
+#	136.5	- onehundred and thirtysix decimal five
+#
+proc playNumber {number} {
+  if {[regexp {(\d+)\.(\d+)?} $number -> integer fraction]} {
+    playNumber $integer;
+    playMsg "Default" "decimal";
+    spellNumber $fraction;
+    return;
+  }
+
+  while {[string length $number] > 0} {
+    set len [string length $number];
+    if {$len == 1} {
+      playMsg "Default" $number;
+      set number "";
+    } elseif {$len % 2 == 0} {
+      playTwoDigitNumber [string range $number 0 1];
+      set number [string range $number 2 end];
+    } else {
+      playThreeDigitNumber [string range $number 0 2];
+      set number [string range $number 3 end];
+    }
+  }
+}
+
+
+proc playTime {hour minute} {
+  set hour [string trimleft $hour " "];
+  set minute [string trimleft $minute " "];
+  
+  if {[string length $hour] == 1} {
+    set hour "0$hour";
+  }
+  playTwoDigitNumber $hour;
+
+  if {$hour > 9 && $minute >= 10} {
+    playMsg "Default" and
+  } else {
+    playSilence 100
+  }
+
+  if {[string length $minute] == 1} {
+    set minute "0$minute";
+  }
+  playTwoDigitNumber $minute;
+  
+  playSilence 100;
+}
+
+
+#
+# This file has not been truncated
+#
